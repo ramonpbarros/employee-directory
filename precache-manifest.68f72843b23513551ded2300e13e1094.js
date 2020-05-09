f3a28d962b7cc34c479b8657cab6160d6b83b38d
Sorrab <ramonpbarros@gmail.com>
2020-05-09 12:03:12 -0700

    Updates

---
+++ precache-manifest.68f72843b23513551ded2300e13e1094.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1b923ea39d4cc30d4af2ec50401659",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "0cfc7accd390b1b5d937",
+    "url": "/employee-directory/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "cfc01cbc2d43a11ea1b7",
+    "url": "/employee-directory/static/css/main.87a86cbb.chunk.css"
+  },
+  {
+    "revision": "0cfc7accd390b1b5d937",
+    "url": "/employee-directory/static/js/2.7c66dbfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.7c66dbfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc01cbc2d43a11ea1b7",
+    "url": "/employee-directory/static/js/main.225586a6.chunk.js"
+  },
+  {
+    "revision": "c938e05d50f8e96cbcb3",
+    "url": "/employee-directory/static/js/runtime-main.a161ee7e.js"
+  }
+]);
